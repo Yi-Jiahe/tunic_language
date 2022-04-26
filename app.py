71117811adf31246f2e9fe1e9624d1be55c206bb
Jiahe <yi.jiahe00@gmail.com>
2022-04-26 20:25:12 +0800

    add Google Cloud logging

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import json
+import logging
 
 from flask import Flask, make_response, request
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
+
+import google.cloud.logging
+client = google.cloud.logging.Client()
+client.setup_logging()
 
 from main import to_phoneme, to_runes
 
@@ -15,17 +20,22 @@ def hello_world():
 
 @app.route('/to-runes', methods=['POST'])
 def translate_to_runes():
-    req_data = json.loads(request.data)
-    str_input = req_data["input"]
-    sentence_as_phonemes = to_phoneme(str_input)
-    runes = [to_runes(x) if isinstance(x, list) else x for x in sentence_as_phonemes]
+    try:
+        req_data = json.loads(request.data)
+        str_input = req_data["input"]
+        logging.info(str_input)
+        sentence_as_phonemes = to_phoneme(str_input)
+        runes = [to_runes(x) if isinstance(x, list) else x for x in sentence_as_phonemes]
 
-    return {
-        'statusCode': 200,
-        'body': {
-            'runes': json.dumps(runes, default=lambda x: list(x) if isinstance(x, set) else x),
+        return {
+            'statusCode': 200,
+            'body': {
+                'runes': json.dumps(runes, default=lambda x: list(x) if isinstance(x, set) else x),
+            }
         }
-    }
+    except Exception as e:
+        logging.error(e)
+
 
 if __name__ == "__main__":
     import os
