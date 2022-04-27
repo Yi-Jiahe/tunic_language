fcf4cd13867ad0822385ffc397f0e5d98ff626e6
Yi-Jiahe <47210363+Yi-Jiahe@users.noreply.github.com>
2022-04-27 18:03:03 +0800

    [CI SKIP] Update Rune

--- client/src/Rune.tsx
+++ client/src/Rune.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Rune.css';
 
-const scale = 1;
-
-const X = scale * 10 * Math.sqrt(2);
-const Y = scale * 10 * 1;
+const X = Math.sqrt(2);
+const Y = 1;
 const marginTop = 1;
 const marginLeft = 1;
-const points = [
+const Points = [
     // Top Diamond
     [2 * X, 0.5 * Y],
     [X, 0],
@@ -29,27 +27,29 @@ const points = [
     [marginLeft, 2.5 * Y],
 ]
 
-interface LineProps {
-    start: number,
-    end: number,
-};
-
-function Line(props: LineProps) {
-    return (
-        <line
-            x1={points[props.start][0]}
-            y1={points[props.start][1] + marginTop}
-            x2={points[props.end][0]}
-            y2={points[props.end][1] + marginTop} />
-    );
-}
-
 interface RuneProps {
     segments: Set<number>,
-    title?: string
+    title?: string,
+    fontSize?: number
 }
 
 export default function Rune(props: RuneProps) {
+    const fontSize = props.fontSize === undefined ? 10 : props.fontSize;
+
+    const points = Points.map((point) => [point[0] * fontSize, point[1] * fontSize])
+
+    const line = (start: number, end: number, rounded?: boolean) => {
+        return (
+            <line
+                x1={points[start][0]}
+                y1={points[start][1] + marginTop}
+                x2={points[end][0]}
+                y2={points[end][1] + marginTop}
+                strokeWidth={fontSize * 1/5}
+                strokeLinecap={rounded ? 'round' : 'butt'} />
+        )
+    }
+
     return (
         <svg width={2 * X} height={3.5 * Y + 2 * marginTop}>
             {
@@ -58,60 +58,58 @@ export default function Rune(props: RuneProps) {
             <g className="vowels">
                 {
                     props.segments.has(1) &&
-                    <Line start={0} end={1} />
+                    line(0, 1)
                 }
                 {
                     props.segments.has(2) &&
-                    <Line start={1} end={2} />
+                    line(1, 2)
                 }
                 {
                     props.segments.has(3) &&
-                    <g><Line start={11} end={12} /><Line start={13} end={14} /></g>
+                    <g>{line(11, 12)}{line(13,14)}</g>
                 }
                 {
                     props.segments.has(4) &&
-                    <Line start={6} end={7} />
+                    line(6, 7)
                 }
                 {
                     props.segments.has(5) &&
-                    <Line start={7} end={4} />
+                    line(7, 4)
                 }
             </g>
 
             <g className="consonants">
                 {
                     props.segments.has(6) &&
-                    <Line start={0} end={3} />
+                    line(0, 3)
                 }
                 {
                     props.segments.has(7) &&
-                    <Line start={1} end={9} />
+                    line(1, 9)
                 }
                 {
                     props.segments.has(8) &&
-                    <Line start={2} end={3} />
+                    line(2, 3)
                 }
                 {
                     props.segments.has(9) &&
-                    <Line start={6} end={5} />
+                    line(6, 5)
                 }
                 {
                     props.segments.has(10) &&
-                    <Line start={7} end={5} />
+                    line(7, 5)
                 }
                 {
                     props.segments.has(11) &&
-                    <Line start={4} end={5} />
+                    line(4, 5)
                 }
             </g>
-
-            <Line start={8} end={10} />
+            
+            line(8, 10)
 
             {
                 props.segments.has(12) &&
-                <circle cx={points[7][0]} cy={points[7][1] + scale * 2.5 + marginTop} r={scale * 2.5} />
+                <circle cx={points[7][0]} cy={points[7][1] + fontSize * 2.5 + marginTop} r={fontSize * 2.5} />
             }
-
-
         </svg>);
 }
