f4e179f481886df6463e5db793aa09052d25169c
Yi-Jiahe <47210363+Yi-Jiahe@users.noreply.github.com>
2022-04-28 13:05:54 +0800

    WIP: Draw runes to canvas

--- client/src/RuneCanvas.tsx
+++ client/src/RuneCanvas.tsx
@@ -1,15 +1,71 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { renderToString } from 'react-dom/server'
 
 interface RuneCanvasProps {
     width: number,
-    height: number
+    height: number,
+    words: Array<JSX.Element>
 }
 
 export default function RuneCanvas(props: RuneCanvasProps) {
-    return (
-        <canvas
-            width={props.width}
-            height={props.height}>
-        </canvas>
-    )
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+    const ret = (
+        <>
+            <canvas
+                ref={canvasRef}
+                width={props.width}
+                height={props.height}>
+            </canvas>
+        </>
+    );
+
+    if (!canvasRef.current) {
+        return ret;
+    }
+
+    const canvas = canvasRef.current;
+
+    const context = canvas.getContext('2d');
+    if (!context) {
+        return ret;
+    }
+    const URL = window.URL || window.webkitURL || window;
+
+    const drawImage = () => {
+        const image = new Image();
+
+        // const blob = new Blob([renderToString(rune)] ,{type:'image/svg+xml;charset=utf-8'});
+        // const blobURL = URL.createObjectURL(blob);
+        // image.src = blobURL;
+
+        context.drawImage(image, x, y, props.width, props.height );
+        x += 100;
+        y += 100;
+    }
+
+
+    let [x, y] = [0, 0];
+
+    props.words.forEach((word) => {
+        const parent = document.createElement("div");
+        parent.innerHTML = renderToString(word);
+        const child = parent.firstChild as Element;
+        
+        if (child.className === "word") {
+            const runes = Array.from(child.children);
+            for (let rune of runes) {
+                console.log("rune", rune);
+            }
+
+        } else if (child.className === "punctuation"){
+            console.log("punctuation", child.innerHTML);
+        }
+    })
+
+    const png = canvas.toDataURL(); // default png
+    const jpeg = canvas.toDataURL('image/jpg');
+    const webp = canvas.toDataURL('image/webp');
+
+    return ret;
 }
--- client/src/TextArea.tsx
+++ client/src/TextArea.tsx
@@ -29,6 +29,25 @@ export default function TextArea(): JSX.Element {
         }, 600)
     }
 
+    const words = tokens.map((token: Array<[Array<number>, Array<string>]> | string, i) => {
+        if (Array.isArray(token)) {
+            return (
+                <span key={i} className="word">
+                    {
+                        token.map((rune: [Array<number>, Array<string>], j) => {
+                            return (
+                                <Rune key={`${i}, ${j}`}
+                                    title={rune[1].join("")}
+                                    segments={new Set(rune[0])} />);
+                        })
+                    }
+                </span>
+            );
+        } else {
+            return <span key={i} className="punctuation">{token}</span>
+        }
+    });
+
     return (
         <>
             <div className='grid-container left-align'>
@@ -52,41 +71,28 @@ export default function TextArea(): JSX.Element {
                         {
                             tokens.length === 0
                                 ? <span className="placeholder">Translation</span>
-                                : tokens.map((token: Array<[Array<number>, Array<string>]> | string, i) => {
-                                    if (Array.isArray(token)) {
-                                        return (
-                                            <span key={i} className="word">
-                                                {
-                                                    token.map((rune: [Array<number>, Array<string>], j) => {
-                                                        return (
-                                                            <Rune key={`${i}, ${j}`}
-                                                                title={rune[1].join("")}
-                                                                segments={new Set(rune[0])} />);
-                                                    })
-                                                }
-                                            </span>
-                                        );
-                                    } else {
-                                        return <span key={i} className="puncuation">{token}</span>
-                                    }
-                                })
+                                : words
                         }
                     </div>
                 </div>
             </div>
             <hr />
 
-            <div className={`modal ${modalHidden ? 'hidden' : ''}`}>
-                <div className="modal-content">
-                    <RuneCanvas
-                        width={600}
-                        height={400} />
-                    <button 
-                        onClick={() => { setModalHidden(true); }}>
-                        Close
-                    </button>
+            {
+                modalHidden === false && <div className="modal">
+                    <div className="modal-content">
+                        <RuneCanvas
+                            width={600}
+                            height={400}
+                            words={words} />
+                        <button
+                            onClick={() => { setModalHidden(true); }}>
+                            Close
+                        </button>
+                    </div>
                 </div>
-            </div>
+            }
+
         </>
 
     )
